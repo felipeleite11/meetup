ee5224bf23e81700ac73644b56279c862884f7e7
Felipe Leite <felipe.leite.nazareth@gmail.com>
2019-09-22 09:06:22 -0300

    Requisitos do Desafio 02 - Iniciando aplicação (acesso ao MySQL com Sequelize, autenticação JWT, criptografia com Bcrypt).

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-app.get('/', (req, res) => {
-    return res.send('Minha primeira versão está online!')
-})
-
-app.listen(3333)
---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '636b6f42911b709759cd08cb1f1f4303',
+    expiresIn: '7d'
+}
